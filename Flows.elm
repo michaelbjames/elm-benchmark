d57f438d42179539b1b507055ae9f62b45ee3304
Michael James <michael@michaelbjames.com>
2014-06-12 11:51:44 +0200

    updated flows

--- Flows.elm
+++ Flows.elm
@@ -3,8 +3,7 @@ module Main where
 import Perf.Runner (..)
 import Perf.Benchmark (..)
 
-{-| Setup. These are some helpful functions
--}
+
 imagePaths : [String]
 imagePaths = map (\x -> "images/" ++ show x ++ ".jpg") [1..12]
 
@@ -14,41 +13,61 @@ directions = [up, down, left, right, inward, outward]
 intToCircle : Int -> Form
 intToCircle n = filled red <| circle <| toFloat n
 
-simpleContent : [Element]
-simpleContent =  map (image 500 100) imagePaths
-              ++ map asText [0..9]
+sampleContent : [Element]
+sampleContent =  map (image 500 100) imagePaths
+              ++ repeat 10 sampleMarkdown
               ++ map (\x -> collage 500 100 [x]) (map intToCircle [96..100])
 
+sampleMarkdown : Element
+sampleMarkdown = [markdown|
+Notice again how text always lines up on 4-space indents (including
+that last line which continues item 3 above). Here's a link to [a
+website](http://foo.bar). Here's a link to a [local
+doc](local-doc.html). Here's a footnote [^1].
+
+[^1]: Footnote text goes here.
+
+Tables can look like this:
+
+size  material      color
+----  ------------  ------------
+9     leather       brown
+10    hemp canvas   natural
+11    glass         transparent
+
+Table: Shoes, their sizes, and what they're made of
+
+(The above is the caption for the table.) Here's a definition list:
+
+apples
+  : Good for making applesauce.
+oranges
+  : Citrus!
+tomatoes
+  : There's no "e" in tomatoe.
+|]
+
+--Adding & Removing
+elemsPerSet : [Int] -> [[Element]]
+elemsPerSet xs = map (\x -> take x sampleContent) xs
+
+flowBenchmark: (String, [Int]) -> Direction -> Benchmark
+flowBenchmark (name,num) d = render (name ++ show d) (flow d) (elemsPerSet num)
+
+flowNames : [(String,[Int])]
+flowNames = [("addToFlow",[1..25]), ("removeFromFlow", reverse [1..25])]
+
+flowStep : (String,[Int]) -> [Benchmark] -> [Benchmark]
+flowStep (name,num) xs = xs ++ (map (flowBenchmark (name,num)) directions)
+
+flows : [Benchmark]
+flows = foldr flowStep [] flowNames
+--Swapping elements in a flow
+
+--Nested Flows
 
 
 
-flowDirection : Direction -> Benchmark
-flowDirection d = staticRender ("flowDirection" ++ show d) (flow d simpleContent)
-flowDirections : [Benchmark]
-flowDirections = map flowDirection [ up, down, up, left, right, inward, outward ]
-
-increasingElems : [[Element]]
-increasingElems = map (\x -> take x simpleContent) [1..25]
-decreasingElems : [[Element]]
-decreasingElems = map (\x -> take x simpleContent) <| reverse [1..25]
-
-directionForIncreasingFlow : Direction -> Benchmark
-directionForIncreasingFlow d = render ("addingToFlow" ++ show d) (flow d) increasingElems
-
-directionForDecreasingFlow : Direction -> Benchmark
-directionForDecreasingFlow d = render ("removingFromFlow" ++ show d) (flow d) decreasingElems
-
-addingToFlow : [Benchmark]
-addingToFlow = map directionForIncreasingFlow directions
-
-removingFromFlow : [Benchmark]
-removingFromFlow = map directionForDecreasingFlow directions
-
-benchmarks : [Benchmark]
-benchmarks = removingFromFlow ++ addingToFlow ++ flowDirections
-
-extraBenchmarks : [Benchmark]
-extraBenchmarks = foldr (\a b -> b ++ repeat 3 a) [] benchmarks
 
 main : Signal Element
-main = run extraBenchmarks
+main = run flows
