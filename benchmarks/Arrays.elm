2a74dae51424f57cb44aa049a02ccce2d214166f
Michael James <michael@michaelbjames.com>
2014-06-16 11:32:53 +0200

    Arrays v1

--- benchmarks/Arrays.elm
+++ benchmarks/Arrays.elm
@@ -13,7 +13,7 @@ emptyBench = let multiplier = 100000
 
 repeatBench = let multiplier = 1000000
                   trials = map (\x -> multiplier * x) [1..10]
-                  repeatWrap i = let _ = Array.repeat i 'a' in ()
+                  repeatWrap i = let _ = Array.repeat i () in ()
               in  logicGroup ("10 repeat arrays") repeatWrap trials
 
 -- We're not testing how fast function application is but how the functions
@@ -25,13 +25,13 @@ fromListBench = let multiplier = 1000000
 
 
 lengthBench = let multiplier = 1000000
-                  trials = map (\x -> Array.repeat (multiplier * x) 0) [1..10]
+                  trials = map (\x -> Array.repeat (multiplier * x) () ) [1..10]
                   lengthWrap xs = let _ = Array.length xs in ()
               in  logicGroup "10 Lengths" lengthWrap trials
 
 
 pushBench = let multiplier = 10000
-                trialData = map (\x -> (x,Array.repeat (multiplier * x) 0)) [1..10]
+                trialData = map (\x -> (x,Array.repeat (multiplier * x) () )) [1..10]
                 pushWrap (x,xs) = let _ = Array.push x xs in ()
             in  logicGroup "10 Pushes" pushWrap trialData
 
@@ -43,16 +43,45 @@ appendBench = let multiplier = 10000
               in  logicGroup "Append of varying differences" appendWrap trialData
 
 getBench = let multiplier = 100
-               trialData = map (\x -> ((multiplier * x), Array.repeat (5 * multiplier) 0) ) [1..5]
+               trialData = map (\x -> ((multiplier * x), Array.repeat (5 * multiplier) () )) [1..5]
                getWrap (position, array) = let _ = Array.get position array in ()
            in  logicGroup "Get at different positions" getWrap trialData
 
 
-sliceBench = let array = Array.repeat 10000 0
+sliceBench = let array = Array.repeat 10000 ()
                  slices = [ (0, 100) , (0, -100), (-100, 900) , (-100, 0)
                           , (500, 501) , (500, 1000)]
                  sliceWrap (start, end) = let _ = Array.slice start end array in ()
             in  logicGroup "Slice" sliceWrap slices
+
+
+toListBench = let multiplier = 1000
+                  trialData = map (\x -> Array.repeat (multiplier * x) () ) [1..10]
+                  toListWrap array = let _ = Array.toList in ()
+              in  logicGroup "toList" toListWrap trialData
+
+
+mapBench = let multiplier = 1000
+               toyFunction = id
+               trialData = map (\x -> Array.repeat (multiplier * x) () ) [1..10]
+               mapWrap array = let _ = Array.map toyFunction array in ()
+           in  logicGroup "map" mapWrap trialData
+
+
+foldlBench = let multiplier = 1000
+                 toyFunction _ _ = ()
+                 trialData = map (\x -> Array.repeat (multiplier * x) () ) [1..10]
+                 foldlWrap array = let _ = Array.foldl toyFunction () array
+             in  logicGroup "foldl" foldlWrap trialData
+
+
+foldrBench = let multiplier = 1000
+                 toyFunction _ _ = ()
+                 trialData = map (\x -> Array.repeat (multiplier * x) () ) [1..10]
+                 foldrWrap array = let _ = Array.foldr toyFunction () array
+             in  logicGroup "foldr" foldrWrap trialData
+
+
 
 
 benchmarks : [Benchmark]
@@ -62,7 +91,11 @@ benchmarks = [ emptyBench
              , lengthBench
              , pushBench
              , appendBench
+             , toListBench
+             , mapBench
+             , foldlBench
+             , foldrBench
              ]
 
 main : Signal Element
-main = run [sliceBench]
+main = run benchmarks
