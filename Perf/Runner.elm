93aa312dba00e5abaea60e481d9a3fdf3fc5e039
Michael James <michael@michaelbjames.com>
2014-06-18 12:36:00 +0200

    added lazy harness

--- Perf/Runner.elm
+++ Perf/Runner.elm
@@ -6,15 +6,8 @@ import Either (..)
 
 type Result = { name:String, times:[Time] }
 
-data PrepBenchmark = PrepLogic String [() -> ()]
-                   | PrepRender String [() -> Element]
-
 run : [Benchmark] -> Signal Element
-run bms =
-    let prepedBenchmark bm = case bm of
-        Logic name tthunks -> PrepLogic name <| map (\thunk -> thunk ()) tthunks
-        Render name tthunks -> PrepRender name <| map (\thunk -> thunk ()) tthunks
-    in  lift display <| Native.Runner.runMany <| map prepedBenchmark bms
+run bms = lift display <| Native.Runner.runMany bms
 
 display : Either Element [Result] -> Element
 display elementString = case elementString of
