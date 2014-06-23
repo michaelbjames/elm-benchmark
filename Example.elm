1cfc14793e07f128e2e98d4cb95cf249f9342bd1
Michael James <michael@michaelbjames.com>
2014-06-23 12:54:57 +0200

    simplified imports

--- Example.elm
+++ Example.elm
@@ -1,17 +1,12 @@
 module Main where
 
-import Perf.Runner (..)
 import Perf.Benchmark (..)
-import Perf.Types (..)
 
 fib : Int -> Int
 fib n =  case n of
           0 -> 1
           1 -> 1
           _ -> fib (n-1) + fib (n-2)
-
---fibWrapper : Int -> (()->())
-fibWrapper n = logicFunction <| fib n
 
 fibMark : Benchmark
 fibMark = logic "high fibonacci" fib [20..30]
