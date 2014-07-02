4cda8e06f02a29b543900b6b65e216bb9720d33c
Michael James <michael@michaelbjames.com>
2014-07-02 12:46:14 +0200

    added fibonacci example

--- Example.elm
+++ Example.elm
@@ -2,28 +2,38 @@ module Main where
 
 import Perf.Benchmark (..)
 
-fib : Int -> Int
-fib n =  case n of
+slowFib : Int -> Int
+slowFib n =  case n of
           0 -> 1
           1 -> 1
-          _ -> fib (n-1) + fib (n-2)
+          _ -> slowFib (n-1) + slowFib (n-2)
 
-fibMark : Benchmark
-fibMark = logic "high fibonacci" fib [20..30]
+phiFib : Int -> Int
+phiFib n = 
+    let sq5 = sqrt 5
+        phi = (sq5 + 1) / 2
+    in  round ((phi ^ toFloat n) / sq5)
+
+slowFibonacci : Benchmark
+slowFibonacci = logic "Fibonacci numbers, recursively, from F(20) to F(30)" slowFib [20..30]
+
+fastFibonacci : Benchmark
+fastFibonacci = logic "Fibonacci numbers, with phi exponentiation, from F(20) to F(30)" phiFib [20..30]
 
 circleWrapper : Color -> Int -> Element
 circleWrapper col n = collage 200 200 [filled col <| circle <| toFloat n]
 
 renderMark : Benchmark
-renderMark = render "Circle" (circleWrapper red) [10..49]
+renderMark = render "Increase the radius of a circle" (circleWrapper red) [10..49]
 
 staticMark : Benchmark
-staticMark = renderStatic "Blue Circle" (circleWrapper blue 100)
+staticMark = renderStatic "Blue Circle, radius 100" (circleWrapper blue 100)
 
 groupMark : [Benchmark]
 groupMark = [ staticMark
-            , fibMark
             , renderMark
+            , slowFibonacci
+            , fastFibonacci
             ]
 
 main : Signal Element
